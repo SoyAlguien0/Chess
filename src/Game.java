1432429f19e38d70256489f7a51f77dfc0c30619
SoyAlguien0 <holayasir@gmail.com>
2025-12-12 11:55:57 +0100

    Adding the logic to set pieces and show the possible moves/kills

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Game {
     private int totalMovements;
@@ -16,21 +15,60 @@ public class Game {
 
     public void initGame(){
         game.initBoard();
-        loopGame();
+        gameLoop();
     }
 
-    public void loopGame(){
+    public void gameLoop(){
         boolean gameOver = false;
         while(!gameOver){
             if (players.length == 2){
                 for (int i = 0; i < players.length; i++) {
+                    Player currentPlayer = players[i];
                     System.out.println("Player "+(i+1)+" turn.");
-                    ArrayList<Piece> availablePieces = players[i].getAvailablePieces();
+
+                    //show available pieces
+                    ArrayList<Piece> availablePieces = currentPlayer.getAvailablePieces();
                     showAvailablePieces(availablePieces);
-                    Piece chosenPiece = players[i].choosePiece(availablePieces);
-                    System.out.println(chosenPiece);
+                    Piece chosenPiece = currentPlayer.choosePiece(availablePieces);
+
+                    //show possible moves from a specific piece
+                    boolean checking = true;
+                    ArrayList<Position> possibleMoves = chosenPiece.getPossibleMoves(!checking);
+                    ArrayList<Position> possibleKills = chosenPiece.getPossibleKills(!checking);
+
+                    Position chosenPosition = null;
+                    if (!possibleMoves.isEmpty() && !possibleKills.isEmpty()) {
+                        System.out.println("1. Kills\n" + //player
+                                "2. moves ");
+                        int chosenPlay = currentPlayer.choosePlay();
+                        if (chosenPlay == 1){
+                            showPossibleMoves(possibleKills);
+                            chosenPosition = currentPlayer.choosePosition(possibleKills);
+                        }else if(chosenPlay == 2) {
+                            showPossibleMoves(possibleMoves);
+                            chosenPosition = currentPlayer.choosePosition(possibleMoves);
+                        }
+                    }
+                    else if (!possibleMoves.isEmpty()){
+                        showPossibleMoves(possibleMoves);
+                        chosenPosition = currentPlayer.choosePosition(possibleMoves);
+                    }
+                    else if (!possibleKills.isEmpty()){
+                        showPossibleMoves(possibleKills);
+                        chosenPosition = currentPlayer.choosePosition(possibleKills);
+                    }
+
+                    //set the piece to a chosen position
+                    setPiece(chosenPiece, chosenPosition);
                 }
             }
+        }
+    }
+
+    public void showPossibleMoves(ArrayList<Position> possibleMoves){
+        for (int i = 0; i < possibleMoves.size(); i++) {
+            Position p = possibleMoves.get(i);
+            System.out.println((i+1)+". "+p);
         }
     }
 
@@ -39,5 +77,10 @@ public class Game {
             Piece p = availablePieces.get(i);
             System.out.println((i+1)+". "+p);
         }
+    }
+
+    public void setPiece(Piece chosenPiece, Position chosenPosition){
+        game.setPiece(chosenPiece ,chosenPosition);
+        game.drawBoard();
     }
 }
